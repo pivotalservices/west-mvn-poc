c44fbc00ca854b9b77410b4341290023d414b803
Yaroslav Novytskyy <ynovytskyy@gmail.com>
2018-05-19 08:47:12 -0400

    using commons-math3 as a lib to  Maven dependency with system scope in PCF

--- src/main/java/com/west/uc/webpoc/WebpocApplication.java
+++ src/main/java/com/west/uc/webpoc/WebpocApplication.java
@@ -1,23 +1,25 @@
 package com.west.uc.webpoc;
 
+import org.apache.commons.math3.primes.Primes;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import sun.util.locale.LocaleUtils;
+
+import java.util.Random;
 
 @SpringBootApplication
 @RestController
 public class WebpocApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WebpocApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WebpocApplication.class, args);
+    }
 
-	@GetMapping("/")
-	public String root() {
-		String testString = "asd123";
-		boolean isAlphaNum = LocaleUtils.isAlphaNumericString(testString);
-		return "Hello world, " + testString + " is alphanum " + isAlphaNum;
-	}
+    @GetMapping("/")
+    public String root() {
+        int random = new Random().nextInt(Integer.MAX_VALUE);
+        int nextPrime = Primes.nextPrime(random);
+        return "Hello world, random [" + random + "] next prime [" + nextPrime + "]";
+    }
 }
